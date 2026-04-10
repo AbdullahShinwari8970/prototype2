500ac81fc06fc53e780e95581607c07e07565983
MrQuotidian123 <124063278+MrQuotidian123@users.noreply.github.com>
2026-04-10 19:19:22 +0100

    removed form dto

--- PROTOTYPE2/src/main/java/com/example/PROTOTYPE2/dto/FormDto.java
+++ PROTOTYPE2/src/main/java/com/example/PROTOTYPE2/dto/FormDto.java
@@ -1,10 +1,10 @@
-package com.example.PROTOTYPE2.dto;
-
-import java.util.List;
-
-public record FormDto(
-        Integer id,
-        String prompt,
-        String type,          // "text" or "multiple_choice"
-        List<String> options  // null/empty if type="text"
-) {}
+//package com.example.PROTOTYPE2.dto;
+//
+//import java.util.List;
+//
+//public record FormDto(
+//        Integer id,
+//        String prompt,
+//        String type,          // "text" or "multiple_choice"
+//        List<String> options  // null/empty if type="text"
+//) {}
